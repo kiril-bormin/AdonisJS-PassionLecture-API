34a49de798da7e03c0e821910949b0aa60400da6
Kiril Bormin <borminnkiril@gmail.com>
2026-04-29 10:16:32 +0200

    feat(vue) : lier l'api avec le frontend

--- backend/app/controllers/books_controller.ts
+++ backend/app/controllers/books_controller.ts
@@ -1,16 +1,37 @@
-import type { HttpContext } from '@adonisjs/core/http'
 import Book from '#models/book'
+import type { HttpContext } from '@adonisjs/core/http'
 
 export default class BooksController {
   /**
    * Display a list of resource
    */
-  async index({}: HttpContext) {
+  async index({ request }: HttpContext) {
     const query = Book.query()
       .preload('author')
       .preload('publisher')
-      .preload('categories')
+      .preload('category')
       .preload('reviews')
+
+    const { page = 1, search, category, author, publisher } = request.qs()
+    const limit = 16
+
+    if (search) {
+      query.where('title', 'like', `%${search}%`)
+    }
+
+    if (category) {
+      query.where('category_id', Number(category))
+    }
+
+    if (author) {
+      query.where('author_id', Number(author))
+    }
+
+    if (publisher) {
+      query.where('publisher_id', Number(publisher))
+    }
+
+    return query.paginate(Number(page), limit)
   }
 
   /**
